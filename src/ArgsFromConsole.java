5e15a4907e0495f5044b5b1e6f79fdb6b6ce941d
MaxHunko <g4ddd888@gmail.com>
2022-12-15 18:41:26 +0200

    first push

--- src/ArgsFromConsole.java
+++ src/ArgsFromConsole.java
@@ -1,6 +1,7 @@
 public class ArgsFromConsole {
     public static void main(String[] args) {
         String tempArgs = "";
+        int asd = 0;
         for (int i = 0; i < args.length; i++) {
             tempArgs = tempArgs + args[i] + " ";
         }
